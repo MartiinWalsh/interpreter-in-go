736aab8aa8dec227f1d4b8345cc5b015430fe694
martinw-dev <martinwalsh.dev@gmail.com>
2023-07-12 21:58:37 +0100

    Initial AST

---
+++ ast/ast.go
@@ -0,0 +1,55 @@
+package ast
+
+import (
+	"github.com/MartiinWalsh/interpreter-in-go/token"
+)
+
+// Every node in the AST has to implement Node interface
+type Node interface {
+	// Used for debugging and testing
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+// This Program Node is the root node of every AST our parser produces
+// Every valid Monkey program is a series of statements
+// Statements are just a slice of AST nodes
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
